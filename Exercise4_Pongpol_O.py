82cc1669362aa86e02ab53b992b5c0c4d9bda0f8
Pongpol Onsiri <goodbetterz.d@gmail.com>
2024-01-16 12:35:07 +0700

    Add Exercise4 file

---
+++ Exercise4_Pongpol_O.py
@@ -0,0 +1,10 @@
+Foundation_English = 75.3
+General_Business = 82.4
+Intro_to_Com_Systems = 90.0
+Computer_Programming = 77.7
+
+print("--------Your Score--------")
+print("Foundation English :", Foundation_English)
+print("General Business :", General_Business)
+print("Introduction to Computer Systems :", Intro_to_Com_Systems)
+print("Computer Programming :", Computer_Programming)
