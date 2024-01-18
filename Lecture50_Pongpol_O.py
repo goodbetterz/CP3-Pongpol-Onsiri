413913b6090ebfb18bf4f515593b1aaef6db3836
Pongpol Onsiri <goodbetterz.d@gmail.com>
2024-01-18 09:40:32 +0700

    Add Lecture50 file

---
+++ Lecture50_Pongpol_O.py
@@ -0,0 +1,16 @@
+def plusNumber(x, y):
+    print(x+y)
+
+def minusNumber(x, y):
+    print(x-y)
+
+def multiplyNumber(x, y):
+    print(x*y)
+
+def divideNumber(x, y):
+    print(x/y)
+
+plusNumber(10, 2)
+minusNumber(10, 2)
+multiplyNumber(10, 2)
+divideNumber(10, 2)
